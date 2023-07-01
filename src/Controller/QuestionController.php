e11f7cb47c097772bcc7100abb05282c59ae1da6
Szeplaki <szeplaki.eva1@gmail.com>
2023-07-01 09:52:01 +0200

    change the text of the labels from English to Hungarian

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,6 +8,7 @@ use App\Form\Type\QuestionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,7 +22,7 @@ class QuestionController extends AbstractController {
 
         $form = $this->createFormBuilder($question)
             ->add('name', TextType::class)
-            ->add('email', TextType::class)
+            ->add('email', EmailType::class)
             ->add('message', TextType::class)
             ->add('save', SubmitType::class, ['label' => 'Send Question'])
             ->getForm();
--- src/Form/Type/QuestionType.php
+++ src/Form/Type/QuestionType.php
@@ -6,6 +6,7 @@ use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,10 +16,26 @@ class QuestionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('email', TextType::class)
-            ->add('message', TextType::class)
-            ->add('save', SubmitType::class)
+            ->add('name', TextType::class, ['label' => 'Neved',
+            'row_attr' => [
+                'class' => 'input-group-text',
+            ],])
+
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail címed',
+                'row_attr' => [
+                    'class' => 'input-group',
+                ],
+            ])
+
+            ->add('message', TextType::class, ['label' => 'Üzenet szövege',
+            'row_attr' => [
+                'class' => 'input-group',
+            ],])
+            ->add('save', SubmitType::class, ['label' => 'Küldés',
+            'row_attr' => [
+                'class' => 'input-group',
+            ],])
         ;
     }
     public function configureOptions(OptionsResolver $resolver): void
